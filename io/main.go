5d9aec4ab37a3691f6d54cba3fa31687def8f689
gorik0 <delavarus@gmail.com>
2024-08-30 22:02:54 +0300

    string_builder

--- io/main.go
+++ io/main.go
@@ -1,46 +1,19 @@
 package main
 
 import (
-	"log"
+	"io"
+	"os"
 	"strings"
-	"unicode/utf8"
 )
 
 func main() {
 
-	//:::: make runa with builder
-	var builder strings.Builder
-	var runa []byte = make([]byte, 10)
-	utf8.EncodeRune(runa, 2025)
-	//:: write to builder
-	builder.Write(runa)
-	println(builder.String())
-	//:: trim from builder '0' runa
-	ru := strings.Trim(builder.String(), string(0))
+	//	:::: string BUIDLER make concat less memory
+	var bi strings.Builder
 
-	//	:::: NOW we have runa, which encode with not a signle byte
+	bi.Write([]byte("egor"))
 
-	nameWithRuna := "egor" + ru
-
-	//	:::: ITERATE THROUGH ALL SYMBOLS
-
-	//::; METH 1
-	for _, r := range nameWithRuna {
-		log.Println(string(r))
-	}
-	//::; METH 2
-	slic := strings.Split(nameWithRuna, "")
-	for _, s := range slic {
-		log.Println(s)
-
-	}
-	//::; METH 3
-	for len(nameWithRuna) > 0 {
-		r, size := utf8.DecodeRuneInString(nameWithRuna)
-		log.Printf("%c", r)
-		nameWithRuna = nameWithRuna[size:]
-	}
-
+	io.Copy(os.Stdout, strings.NewReader(bi.String()))
 }
 
 //println(strings.Index(name, "e"))
