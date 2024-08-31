d92768c2b17c436868f4c2706bb59c45a78cb1af
gorik0 <delavarus@gmail.com>
2024-08-31 15:49:42 +0300

    writer_explain

--- io/main.go
+++ io/main.go
@@ -1,45 +1,33 @@
 package main
 
 import (
-	"io"
+	"bufio"
 	"log"
-	"time"
+	"os"
+	"strings"
 )
 
+type egor int
+
+func (e egor) Write(data []byte) (n int, err error) {
+	log.Println(data)
+	return len(data), nil
+}
 func main() {
+	//::: bufio provide Buffered writer/reader to reduce memory usage , amount of sys call
 
-	//	::: MULTI reader
-	//	::: MULTI writer
-	r, w := io.Pipe()
+	//	uth it's ntihing just a simple []byte, when it's overflowed it write to indoor writer
 
-	go writeToWriter(w)
-	listeReader(r)
+	buw := bufio.NewWriterSize(os.Stdout, 3)
+	buw.WriteString("sdHEL")
+	bi := strings.Builder{}
 
-}
+	//:::; reset this buffer, free buffer size and change inner writer (free data is lost, if not flushed)
+	buw.Reset(&bi)
 
-func listeReader(r *io.PipeReader) {
-	for {
-		all, err := io.ReadAll(r)
-		if err != nil {
-			log.Printf("Error reading from Reader: %v", err)
-			return
-		}
-		log.Printf("From reader: %s", string(all))
+	buw.WriteString("and egor")
 
-	}
-
-}
-
-func writeToWriter(w *io.PipeWriter) {
-	for {
-		time.Sleep(time.Second)
-		write, err := w.Write([]byte("Helo, goriko!!!"))
-		if err != nil {
-			log.Printf("error writing to pipe: %v", err)
-			return
-		}
-
-		log.Printf("Has been written to pipe: %v", write)
-	}
+	print(bi.String())
+	//io.WriteString(bufferedWriter, "egoo")
 
 }
