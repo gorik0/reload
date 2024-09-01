5923c9c13a51c54a5a42219a81f90c2745d8216d
gorik0 <delavarus@gmail.com>
2024-09-01 17:59:51 +0300

    add_custom_reader

--- io/main.go
+++ io/main.go
@@ -2,32 +2,54 @@ package main
 
 import (
 	"bufio"
+	"bytes"
+	"io"
 	"log"
-	"os"
-	"strings"
 )
 
 type egor int
 
-func (e egor) Write(data []byte) (n int, err error) {
-	log.Println(data)
-	return len(data), nil
+func (e egor) Read(p []byte) (n int, err error) {
+	log.Println("read invoked")
+	p[0] = 99
+	switch {
+	case len(p) > 10:
+		return len(p), io.EOF
+	default:
+		return len(p), nil
+	}
 }
+
 func main() {
 	//::: bufio provide Buffered writer/reader to reduce memory usage , amount of sys call
 
 	//	uth it's ntihing just a simple []byte, when it's overflowed it write to indoor writer
+	//var reader egor
+	//var strR = strings.NewReader("egor")
+	//::: min size  16 byte
+	//::: when request to reader first, invoke Read from internal reeader, then again, if buffer is empty
 
-	buw := bufio.NewWriterSize(os.Stdout, 3)
-	buw.WriteString("sdHEL")
-	bi := strings.Builder{}
+	//log.Println(bur.WriteTo(os.Stdout))
+	//bur.Discard(21)
+	reader := bytes.NewReader([]byte{112, 2, 31, 2, 23, 12, 12, 233})
+	bur := bufio.NewReaderSize(reader, 3)
+	//::: at most one call to internal reader
 
-	//:::; reset this buffer, free buffer size and change inner writer (free data is lost, if not flushed)
-	buw.Reset(&bi)
+	line, err := bur.ReadBytes(2)
+	if err != nil {
+		log.Fatal(err)
+	}
+	i := make([]byte, 1)
+	bur.Read(i)
+	log.Println(i)
+	line2, err := bur.ReadBytes(2)
+	bur.Discard(5)
 
-	buw.WriteString("and egor")
-
-	print(bi.String())
-	//io.WriteString(bufferedWriter, "egoo")
-
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println("line ::: ", line)
+	log.Println("line2 ::: ", line2)
+	//bur.WriteTo(os.Stdout)
+	//println([]byte(string('n')))
 }
